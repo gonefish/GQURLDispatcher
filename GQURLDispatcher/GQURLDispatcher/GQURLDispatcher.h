952f461d7df57ae0023efc5c92a5f7b4391a67c4
Qian GuoQiang <gonefish@gmail.com>
2014-12-23 07:36:04 +0800

    添加注释

--- GQURLDispatcher/GQURLDispatcher/GQURLDispatcher.h
+++ GQURLDispatcher/GQURLDispatcher/GQURLDispatcher.h
@@ -17,14 +17,41 @@
 
 + (instancetype)sharedInstance;
 
+/**
+ *  调用dispatchURL:withObject:，withObject传递nil
+ *
+ */
 - (BOOL)dispatchURL:(NSURL *)url;
 
+/**
+ *  核心方法，调用时会依次将URL和自定义对象传递给匹配的Responder响应
+ *
+ *  @param url      URL对象
+ *  @param anObject 自定义的参数
+ *
+ *  @return 如果有相应的响应者处理则返回YES，否则为NO
+ */
 - (BOOL)dispatchURL:(NSURL *)url withObject:(id)anObject;
 
+/**
+ *  返回所有的响应者
+ *
+ *  @return 响应者数组
+ */
 - (NSArray *)responders;
 
+/**
+ *  注册响应者实例
+ *
+ *  @param responder 实现GQURLResponder的实例
+ */
 - (void)registerResponder:(id <GQURLResponder>)responder;
 
+/**
+ *  取消注册响应者实例
+ *
+ *  @param responder 实现GQURLResponder的实例
+ */
 - (void)unregisterResponder:(id <GQURLResponder>)responder;
 
 @end
@@ -33,6 +60,11 @@
 
 @optional
 
+/**
+ *  是否使用该响应者处理URL
+ *
+ *  @return 如果返回NO，则该响应者不处理URL
+ */
 - (BOOL)urlDispatcher:(GQURLDispatcher *)urlDispatcher shouldWithResponder:(id <GQURLResponder>)responder handleURL:(NSURL *)aURL object:(id)anObject;
 
 - (void)urlDispatcher:(GQURLDispatcher *)urlDispatcher didWithResponder:(id <GQURLResponder>)responder handleURL:(NSURL *)aURL object:(id)anObject;
